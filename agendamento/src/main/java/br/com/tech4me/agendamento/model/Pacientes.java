57d3ac1bca1fe01005ad3fd5a290ef4283091085
kesiaamrl <kesiacamaral@gmail.com>
2023-08-18 15:40:44 -0300

    agendamento

--- agendamento/src/main/java/br/com/tech4me/agendamento/model/Pacientes.java
+++ agendamento/src/main/java/br/com/tech4me/agendamento/model/Pacientes.java
@@ -1,5 +1,18 @@
 package br.com.tech4me.agendamento.model;
 
 public class Pacientes {
-    
+    private String nome;
+    private int idade;
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
 }
